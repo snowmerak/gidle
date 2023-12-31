b396ed413b7428312af6c97affe872c9132495b4
snowmerak <snowmerak@outlook.com>
2023-12-31 15:06:20 +0900

    make snake to camel

--- strconv.go
+++ strconv.go
@@ -19,3 +19,19 @@ func SnakeToPascal(s string) string {
 
 	return sb.String()
 }
+
+func SnakeToCamel(s string) string {
+	sb := strings.Builder{}
+	sb.Grow(len(s))
+
+	for i := range s {
+		if s[i] == '_' && i+1 < len(s) && s[i+1] >= 'a' && s[i+1] <= 'z' {
+			sb.WriteByte(s[i+1] - 32)
+			i++
+		} else {
+			sb.WriteByte(s[i])
+		}
+	}
+
+	return sb.String()
+}
