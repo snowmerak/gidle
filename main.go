e98b2fe5a8c05bdb53c1d82c6782ec0b4468dbbb
snowmerak <snowmerak@outlook.com>
2023-12-31 15:15:34 +0900

    set flag package

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"flag"
 	"os"
+	"path/filepath"
 
 	"github.com/alecthomas/participle/v2"
 )
 
+const (
+	LanguageGo         = "go"
+	LanguageDart       = "dart"
+	LanguageTypeScript = "ts"
+	LanguageRust       = "rust"
+)
+
 func main() {
-	data, err := os.ReadFile("./test.gidle")
+	inputFile := flag.String("i", "", "input file")
+	outputFile := flag.String("o", "", "output file")
+	lang := flag.String("l", "", "output language")
+	flag.Parse()
+
+	if *inputFile == "" || *outputFile == "" || *lang == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	data, err := os.ReadFile(*inputFile)
 	if err != nil {
 		panic(err)
 	}
@@ -19,7 +38,25 @@ func main() {
 		panic(err)
 	}
 
-	if err := NewGoGenerator().Generate("test.go", values); err != nil {
+	if err := os.MkdirAll(filepath.Dir(*outputFile), 0755); err != nil {
+		panic(err)
+	}
+
+	var generator Generator
+	switch *lang {
+	case LanguageGo:
+		generator = NewGoGenerator()
+	case LanguageDart:
+		generator = NewDartGenerator()
+	// case LanguageTypeScript:
+	// 	generator = NewTypeScriptGenerator()
+	// case LanguageRust:
+	// 	generator = NewRustGenerator()
+	default:
+		panic("unknown language")
+	}
+
+	if err := generator.Generate(*outputFile, values); err != nil {
 		panic(err)
 	}
 }
