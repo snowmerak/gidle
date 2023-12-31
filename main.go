0236ea6b53a5fcb2c336dff883976fe1fc597419
snowmerak <snowmerak@outlook.com>
2023-12-31 14:06:39 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,37 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/alecthomas/participle/v2"
 )
 
-const grammar = `
-object Person {
-    string name
-    int32 age
-    list of string friends
-    map string for string properties
-}
-
-enum CASE for uint8 {
-    UPPER = 0
-    LOWER = 1
-}
-
-const BOUNDARY for int32 {
-    MAX = 100
-    MIN = 0
-}
-`
-
 func main() {
-	parser := participle.MustBuild[Grammar]()
-
-	v, err := parser.ParseString("", grammar)
+	data, err := os.ReadFile("./test.gidle")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%#v\n", v)
+	parser := participle.MustBuild[Grammar]()
+
+	values, err := parser.ParseBytes("", data)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := NewGoGenerator().Generate("test.go", values); err != nil {
+		panic(err)
+	}
 }
